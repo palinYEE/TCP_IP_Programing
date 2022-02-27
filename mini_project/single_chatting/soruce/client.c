159561bd42547033d53d1b129224580913dcc2d9
윤영진 <alwns28@kookmin.ac.kr>
2022-02-27 20:28:55 +0900

    [codes] chatting program client structure setting

---
+++ mini_project/single_chatting/soruce/client.c
@@ -0,0 +1,51 @@
+/*
+ * author: YYJ
+ * date: 2022.02.27
+ * discription: one to one chatting program client
+ */
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+#include<arpa/inet.h>
+#include<netinet/in.h>
+#include<sys/socket.h>
+
+/* TODO: 헤더 파일을 만들어서 하나로 관리하기 */
+#define SERVER_PORT 9000
+#define SERVER_IP "192.168.64.5"
+
+int main()
+{
+    int c_socket; 
+    struct sockaddr_in c_addr;
+    int len;
+    int n;
+
+    char rcvBuffer[BUFSIZ];
+
+    /* 소켓 생성 */
+    c_socket = socket(PF_INET, SOCK_STREAM, 0);
+    /* 구조체 셋팅 */
+    memset(&c_socket, 0, sizeof(c_socket));
+    c_addr.sin_family = AF_INET;
+    c_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+    c_addr.sin_port = htons(SERVER_PORT);
+    /* 소켓을 서버에 연결 */
+    len = sizeof(c_addr);
+    if(connect(c_socket, (struct sockaddr*)&c_addr, (socklen_t)len) == -1)
+    {
+        printf("[CLIENT] - [ERROR] : Fail Connect\n"); /* TODO: 에러 출력 함수 하나로 만들기 */
+        return -1;
+    }
+    /* 서비스 요청과 처리 */
+    n = read(c_socket, rcvBuffer, sizeof(rcvBuffer));
+    if(n < 0)
+    {
+        printf("[CLIETN] - [ERROR] : Empty rcvBuffer\n");
+        return -1;
+    } 
+
+    /* 이 사이에 내가 만들려고 하는 서비스를 넣으면 될 듯 */
+
+    close(c_socket);
+}
